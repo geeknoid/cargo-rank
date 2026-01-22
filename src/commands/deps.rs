geeknoid/cargo-aprz

Initial checkin

src/commands/deps.rs
@@ -1,0 +1,211 @@
+use super::common::{Common, CommonArgs};
+use anyhow::{Context, Result, bail};
+use cargo_metadata::{CargoOpt, DependencyKind, Package, PackageId};
+use cargo_rank::facts::CrateRef;
+use cargo_rank::misc::DependencyType;
+use clap::Parser;
+use std::collections::{HashMap, HashSet};
+
+#[derive(Parser, Debug)]
+pub struct DepsArgs {
+    /// Comma-separated list of dependency types being inspected [defaults: all types]
+    #[arg(long = "dependency-types", value_delimiter = ',', value_name = "TYPES")]
+    pub dependency_types: Option<Vec<DependencyType>>,
+
+    /// Space or comma separated list of features to activate
+    #[arg(short = 'F', long, value_name = "FEATURES", help_heading = "Feature Selection")]
+    pub features: Vec<String>,
+
+    /// Activate all available features
+    #[arg(long, help_heading = "Feature Selection")]
+    pub all_features: bool,
+
+    /// Do not activate the `default` feature
+    #[arg(long, help_heading = "Feature Selection")]
+    pub no_default_features: bool,
+
+    /// Process only the specified package
+    #[arg(short = 'p', long, value_name = "SPEC", help_heading = "Package Selection")]
+    pub package: Vec<String>,
+
+    /// Process all packages in the workspace
+    #[arg(long, help_heading = "Package Selection")]
+    pub workspace: bool,
+
+    #[command(flatten)]
+    pub common: CommonArgs,
+}
+
+pub async fn process_dependencies(args: &DepsArgs) -> Result<()> {
+    let mut common = Common::new(&args.common).await?;
+
+    // Configure features on the metadata command based on command-line options
+    if args.all_features {
+        _ = common.metadata_cmd.features(CargoOpt::AllFeatures);
+    } else {
+        if args.no_default_features {
+            _ = common.metadata_cmd.features(CargoOpt::NoDefaultFeatures);
+        }
+
+        if !args.features.is_empty() {
+            _ = common.metadata_cmd.features(CargoOpt::SomeFeatures(args.features.clone()));
+        }
+    }
+
+    let metadata = common.metadata_cmd.exec().context("unable to retrieve workspace metadata")?;
+    let all_packages: HashMap<_, _> = metadata.packages.iter().map(|p| (&p.id, p)).collect();
+
+    // Validate package names if specified
+    if !args.package.is_empty() {
+        let workspace_packages: Vec<_> = metadata
+            .workspace_members
+            .iter()
+            .filter_map(|id| all_packages.get(id).map(|p| &p.name))
+            .collect();
+
+        for pkg_name in &args.package {
+            if !workspace_packages.iter().any(|&name| name == pkg_name) {
+                bail!("package '{pkg_name}' not found in workspace");
+            }
+        }
+    }
+
+    if !args.package.is_empty() {
+        process_packages(
+            args,
+            &common,
+            &all_packages,
+            metadata
+                .workspace_members
+                .iter()
+                .filter_map(|id| all_packages.get(id).copied())
+                .filter(|p| args.package.contains(&p.name)),
+        )
+        .await
+    } else if args.workspace {
+        process_packages(
+            args,
+            &common,
+            &all_packages,
+            metadata.workspace_members.iter().filter_map(|id| all_packages.get(id).copied()),
+        )
+        .await
+    } else if let Some(root) = metadata.root_package() {
+        process_packages(args, &common, &all_packages, core::iter::once(root)).await
+    } else {
+        // Virtual workspace, default to all members
+        process_packages(
+            args,
+            &common,
+            &all_packages,
+            metadata.workspace_members.iter().filter_map(|id| all_packages.get(id).copied()),
+        )
+        .await
+    }
+}
+
+async fn process_packages<'a>(
+    args: &DepsArgs,
+    common: &Common,
+    all_packages: &HashMap<&'a PackageId, &'a Package>,
+    target_packages: impl Iterator<Item = &'a Package>,
+) -> Result<()> {
+    let should_process_std = args
+        .dependency_types
+        .as_ref()
+        .is_none_or(|d| d.is_empty() || d.contains(&DependencyType::Standard));
+    let should_process_dev = args
+        .dependency_types
+        .as_ref()
+        .is_none_or(|d| d.is_empty() || d.contains(&DependencyType::Dev));
+    let should_process_build = args
+        .dependency_types
+        .as_ref()
+        .is_none_or(|d| d.is_empty() || d.contains(&DependencyType::Build));
+
+    // Collect all (CrateId, dependency_type) pairs, preserving duplicates
+    let mut crate_dep_pairs: Vec<(CrateRef, DependencyType)> = Vec::new();
+
+    for package in target_packages {
+        if should_process_std {
+            crate_dep_pairs.extend(build_transitive_deps(
+                all_packages,
+                package
+                    .dependencies
+                    .iter()
+                    .filter(|d| d.kind == DependencyKind::Normal)
+                    .map(|d| d.name.as_str()),
+                DependencyType::Standard,
+            ));
+        }
+
+        if should_process_dev {
+            crate_dep_pairs.extend(build_transitive_deps(
+                all_packages,
+                package
+                    .dependencies
+                    .iter()
+                    .filter(|d| d.kind == DependencyKind::Development)
+                    .map(|d| d.name.as_str()),
+                DependencyType::Dev,
+            ));
+        }
+
+        if should_process_build {
+            crate_dep_pairs.extend(build_transitive_deps(
+                all_packages,
+                package
+                    .dependencies
+                    .iter()
+                    .filter(|d| d.kind == DependencyKind::Build)
+                    .map(|d| d.name.as_str()),
+                DependencyType::Build,
+            ));
+        }
+    }
+
+    // Fetch facts for each (crate_ref, dep_type) pair
+    let facts = common
+        .process_crates(crate_dep_pairs.iter().map(|(crate_ref, _)| crate_ref.clone()))
+        .await?;
+
+    // Pair each CrateFacts with its corresponding dependency type and report
+    common.report(facts.into_iter().zip(crate_dep_pairs.into_iter().map(|(_, dep_type)| dep_type)))
+}
+
+/// Build the transitive closure of dependencies starting from `initial_deps`
+fn build_transitive_deps<'a>(
+    all_packages: &HashMap<&'a PackageId, &'a Package>,
+    initial_deps: impl IntoIterator<Item = &'a str>,
+    dependency_type: DependencyType,
+) -> HashSet<(CrateRef, DependencyType)> {
+    // Convert DependencyType to DependencyKind for filtering
+    let dependency_kind = match dependency_type {
+        DependencyType::Standard => DependencyKind::Normal,
+        DependencyType::Dev => DependencyKind::Development,
+        DependencyType::Build => DependencyKind::Build,
+    };
+
+    let mut result = HashSet::new();
+    let mut queue: Vec<&str> = initial_deps.into_iter().collect();
+    let mut visited_names = HashSet::new();
+
+    while let Some(dep_name) = queue.pop() {
+        if !visited_names.insert(dep_name) {
+            continue; // Already processed
+        }
+
+        // Find the package for this dependency
+        if let Some(pkg) = all_packages.values().find(|p| p.name == dep_name) {
+            _ = result.insert((CrateRef::new(&pkg.name, Some(pkg.version.clone())), dependency_type));
+
+            for dep in &pkg.dependencies {
+                if dep.kind == dependency_kind {
+                    queue.push(dep.name.as_str());
+                }
+            }
+        }
+    }
+
+    result
+}

src/facts/crate_ref.rs
@@ -1,0 +1,76 @@
+use crate::facts::CrateSpec;
+use anyhow::{Context, Result};
+use core::fmt::{Display, Formatter, Result as FmtResult};
+use core::str::FromStr;
+use semver::Version;
+use std::sync::Arc;
+
+/// A crate identifier consisting of a name and optional version
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct CrateRef {
+    name: Arc<str>,
+    version: Option<Arc<Version>>,
+}
+
+impl CrateRef {
+    /// Create a new crate ID with name and optional version
+    #[must_use]
+    pub fn new(name: impl AsRef<str>, version: Option<Version>) -> Self {
+        Self {
+            name: Arc::from(name.as_ref()),
+            version: version.map(Arc::new),
+        }
+    }
+
+    #[must_use]
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    #[must_use]
+    pub fn version(&self) -> Option<&Version> {
+        self.version.as_deref()
+    }
+
+    /// Get a clone of the name Arc
+    #[must_use]
+    pub fn name_arc(&self) -> Arc<str> {
+        Arc::clone(&self.name)
+    }
+
+    /// Get a clone of the version Arc if present (cheap pointer clone, no Version allocation)
+    #[must_use]
+    pub fn version_arc(&self) -> Option<Arc<Version>> {
+        self.version.as_ref().map(Arc::clone)
+    }
+
+    /// Convert to a `CrateSpec` by cloning Arc pointers (no allocation)
+    #[must_use]
+    pub fn to_spec(&self) -> Option<CrateSpec> {
+        Some(CrateSpec::from_arcs(Arc::clone(&self.name), Arc::clone(self.version.as_ref()?)))
+    }
+}
+
+impl FromStr for CrateRef {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        if let Some((name, version_str)) = s.split_once('@') {
+            let version =
+                Version::parse(version_str).with_context(|| format!("invalid version '{version_str}' in crate specifier '{s}'"))?;
+            Ok(Self::new(name, Some(version)))
+        } else {
+            Ok(Self::new(s, None))
+        }
+    }
+}
+
+impl Display for CrateRef {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "{}", self.name())?;
+        if let Some(version) = self.version() {
+            write!(f, "@{version}")?;
+        }
+        Ok(())
+    }
+}

src/ranking/ranker.rs
@@ -1,0 +1,100 @@
+//! Scoring logic for evaluating crate quality.
+
+use crate::config::Config;
+use crate::facts::CrateFacts;
+use crate::metrics::{Metric, MetricCategory};
+use crate::misc::DependencyType;
+use crate::ranking::{PolicyOutcome, metric_calculator};
+use core::cell::RefCell;
+use core::hash::BuildHasher;
+use std::collections::HashMap;
+
+/// Result of ranking a crate
+#[derive(Debug)]
+pub struct RankingOutcome {
+    pub overall_score: f64,
+    pub category_scores: HashMap<MetricCategory, f64>,
+    pub details: HashMap<Metric, PolicyOutcome>,
+    pub dependency_type: DependencyType,
+}
+
+/// Ranker evaluates crate quality based on configured policies
+#[derive(Debug)]
+pub struct Ranker<'a> {
+    config: &'a Config,
+    policy_outcomes: RefCell<HashMap<Metric, PolicyOutcome>>,
+}
+
+impl<'a> Ranker<'a> {
+    /// Create a new ranker with the given configuration
+    #[must_use]
+    pub fn new(config: &'a Config) -> Self {
+        Self {
+            config,
+            policy_outcomes: RefCell::new(HashMap::new()),
+        }
+    }
+
+    /// Rank a crate based on multiple quality criteria
+    pub fn rank(&self, facts: &CrateFacts, dependency_type: DependencyType) -> RankingOutcome {
+        let mut policy_outcomes = self.policy_outcomes.borrow_mut();
+
+        policy_outcomes.clear();
+        metric_calculator::calculate(self.config, facts, dependency_type, &mut policy_outcomes);
+
+        let mut total_points = 0.0;
+        let mut category_points: HashMap<MetricCategory, f64> = HashMap::new();
+        let mut category_counts: HashMap<MetricCategory, usize> = HashMap::new();
+
+        for (metric, outcome) in policy_outcomes.iter() {
+            let category = metric.category();
+            let points = match outcome {
+                PolicyOutcome::Match(points, _info) => *points,
+                PolicyOutcome::NoMatch(_reason) => 0.0,
+            };
+            total_points += points;
+            *category_points.entry(category).or_insert(0.0) += points;
+            *category_counts.entry(category).or_insert(0) += 1;
+        }
+
+        let score = if policy_outcomes.is_empty() {
+            0.0
+        } else {
+            #[expect(clippy::cast_precision_loss, reason = "Precision loss acceptable for score calculation")]
+            let avg = total_points / policy_outcomes.len() as f64;
+            (avg * 100.0).round() / 100.0
+        };
+
+        // Compute average score per category
+        let mut category_scores = HashMap::new();
+        for (category, total_points) in category_points {
+            if let Some(&count) = category_counts.get(&category)
+                && count > 0
+            {
+                #[expect(clippy::cast_precision_loss, reason = "Precision loss acceptable for score calculation")]
+                let avg = total_points / count as f64;
+                let rounded = (avg * 100.0).round() / 100.0;
+                _ = category_scores.insert(category, rounded);
+            }
+        }
+
+        RankingOutcome {
+            overall_score: score,
+            category_scores,
+            details: policy_outcomes.clone(),
+            dependency_type,
+        }
+    }
+}
+
+/// Extract failure reasons from policy results
+#[must_use]
+pub fn extract_reasons<S: BuildHasher>(details: &HashMap<Metric, PolicyOutcome, S>) -> Vec<String> {
+    let mut reasons = Vec::new();
+    for outcome in details.values() {
+        if let PolicyOutcome::NoMatch(reason) = outcome {
+            reasons.push(reason.clone());
+        }
+    }
+    reasons
+}

geeknoid/cargo-rank

src/commands/deps.rs
@@ -1,0 +1,212 @@
+use super::common::{Common, CommonArgs};
+use cargo_metadata::{CargoOpt, DependencyKind, Package, PackageId};
+use cargo_rank::Result;
+use cargo_rank::facts::CrateRef;
+use cargo_rank::misc::DependencyType;
+use clap::Parser;
+use ohno::{IntoAppError, bail};
+use std::collections::{HashMap, HashSet};
+
+#[derive(Parser, Debug)]
+pub struct DepsArgs {
+    /// Comma-separated list of dependency types being inspected [defaults: all types]
+    #[arg(long = "dependency-types", value_delimiter = ',', value_name = "TYPES")]
+    pub dependency_types: Option<Vec<DependencyType>>,
+
+    /// Space or comma separated list of features to activate
+    #[arg(short = 'F', long, value_name = "FEATURES", help_heading = "Feature Selection")]
+    pub features: Vec<String>,
+
+    /// Activate all available features
+    #[arg(long, help_heading = "Feature Selection")]
+    pub all_features: bool,
+
+    /// Do not activate the `default` feature
+    #[arg(long, help_heading = "Feature Selection")]
+    pub no_default_features: bool,
+
+    /// Process only the specified package
+    #[arg(short = 'p', long, value_name = "SPEC", help_heading = "Package Selection")]
+    pub package: Vec<String>,
+
+    /// Process all packages in the workspace
+    #[arg(long, help_heading = "Package Selection")]
+    pub workspace: bool,
+
+    #[command(flatten)]
+    pub common: CommonArgs,
+}
+
+pub async fn process_dependencies(args: &DepsArgs) -> Result<()> {
+    let mut common = Common::new(&args.common).await?;
+
+    // Configure features on the metadata command based on command-line options
+    if args.all_features {
+        _ = common.metadata_cmd.features(CargoOpt::AllFeatures);
+    } else {
+        if args.no_default_features {
+            _ = common.metadata_cmd.features(CargoOpt::NoDefaultFeatures);
+        }
+
+        if !args.features.is_empty() {
+            _ = common.metadata_cmd.features(CargoOpt::SomeFeatures(args.features.clone()));
+        }
+    }
+
+    let metadata = common.metadata_cmd.exec().into_app_err("unable to retrieve workspace metadata")?;
+    let all_packages: HashMap<_, _> = metadata.packages.iter().map(|p| (&p.id, p)).collect();
+
+    // Validate package names if specified
+    if !args.package.is_empty() {
+        let workspace_packages: Vec<_> = metadata
+            .workspace_members
+            .iter()
+            .filter_map(|id| all_packages.get(id).map(|p| &p.name))
+            .collect();
+
+        for pkg_name in &args.package {
+            if !workspace_packages.iter().any(|&name| name == pkg_name) {
+                bail!("package '{pkg_name}' not found in workspace");
+            }
+        }
+    }
+
+    if !args.package.is_empty() {
+        process_packages(
+            args,
+            &common,
+            &all_packages,
+            metadata
+                .workspace_members
+                .iter()
+                .filter_map(|id| all_packages.get(id).copied())
+                .filter(|p| args.package.contains(&p.name)),
+        )
+        .await
+    } else if args.workspace {
+        process_packages(
+            args,
+            &common,
+            &all_packages,
+            metadata.workspace_members.iter().filter_map(|id| all_packages.get(id).copied()),
+        )
+        .await
+    } else if let Some(root) = metadata.root_package() {
+        process_packages(args, &common, &all_packages, core::iter::once(root)).await
+    } else {
+        // Virtual workspace, default to all members
+        process_packages(
+            args,
+            &common,
+            &all_packages,
+            metadata.workspace_members.iter().filter_map(|id| all_packages.get(id).copied()),
+        )
+        .await
+    }
+}
+
+async fn process_packages<'a>(
+    args: &DepsArgs,
+    common: &Common,
+    all_packages: &HashMap<&'a PackageId, &'a Package>,
+    target_packages: impl Iterator<Item = &'a Package>,
+) -> Result<()> {
+    let should_process_std = args
+        .dependency_types
+        .as_ref()
+        .is_none_or(|d| d.is_empty() || d.contains(&DependencyType::Standard));
+    let should_process_dev = args
+        .dependency_types
+        .as_ref()
+        .is_none_or(|d| d.is_empty() || d.contains(&DependencyType::Dev));
+    let should_process_build = args
+        .dependency_types
+        .as_ref()
+        .is_none_or(|d| d.is_empty() || d.contains(&DependencyType::Build));
+
+    // Collect all (CrateId, dependency_type) pairs, preserving duplicates
+    let mut crate_dep_pairs: Vec<(CrateRef, DependencyType)> = Vec::new();
+
+    for package in target_packages {
+        if should_process_std {
+            crate_dep_pairs.extend(build_transitive_deps(
+                all_packages,
+                package
+                    .dependencies
+                    .iter()
+                    .filter(|d| d.kind == DependencyKind::Normal)
+                    .map(|d| d.name.as_str()),
+                DependencyType::Standard,
+            ));
+        }
+
+        if should_process_dev {
+            crate_dep_pairs.extend(build_transitive_deps(
+                all_packages,
+                package
+                    .dependencies
+                    .iter()
+                    .filter(|d| d.kind == DependencyKind::Development)
+                    .map(|d| d.name.as_str()),
+                DependencyType::Dev,
+            ));
+        }
+
+        if should_process_build {
+            crate_dep_pairs.extend(build_transitive_deps(
+                all_packages,
+                package
+                    .dependencies
+                    .iter()
+                    .filter(|d| d.kind == DependencyKind::Build)
+                    .map(|d| d.name.as_str()),
+                DependencyType::Build,
+            ));
+        }
+    }
+
+    // Fetch facts for each (crate_ref, dep_type) pair
+    let facts = common
+        .process_crates(crate_dep_pairs.iter().map(|(crate_ref, _)| crate_ref.clone()))
+        .await?;
+
+    // Pair each CrateFacts with its corresponding dependency type and report
+    common.report(facts.into_iter().zip(crate_dep_pairs.into_iter().map(|(_, dep_type)| dep_type)))
+}
+
+/// Build the transitive closure of dependencies starting from `initial_deps`
+fn build_transitive_deps<'a>(
+    all_packages: &HashMap<&'a PackageId, &'a Package>,
+    initial_deps: impl IntoIterator<Item = &'a str>,
+    dependency_type: DependencyType,
+) -> HashSet<(CrateRef, DependencyType)> {
+    // Convert DependencyType to DependencyKind for filtering
+    let dependency_kind = match dependency_type {
+        DependencyType::Standard => DependencyKind::Normal,
+        DependencyType::Dev => DependencyKind::Development,
+        DependencyType::Build => DependencyKind::Build,
+    };
+
+    let mut result = HashSet::new();
+    let mut queue: Vec<&str> = initial_deps.into_iter().collect();
+    let mut visited_names = HashSet::new();
+
+    while let Some(dep_name) = queue.pop() {
+        if !visited_names.insert(dep_name) {
+            continue; // Already processed
+        }
+
+        // Find the package for this dependency
+        if let Some(pkg) = all_packages.values().find(|p| p.name == dep_name) {
+            _ = result.insert((CrateRef::new(&pkg.name, Some(pkg.version.clone())), dependency_type));
+
+            for dep in &pkg.dependencies {
+                if dep.kind == dependency_kind {
+                    queue.push(dep.name.as_str());
+                }
+            }
+        }
+    }
+
+    result
+}

src/facts/crate_ref.rs
@@ -1,0 +1,91 @@
+use crate::facts::CrateSpec;
+use core::fmt::{Display, Formatter, Result as FmtResult};
+use core::str::FromStr;
+use semver::Version;
+use std::sync::Arc;
+
+/// A crate identifier consisting of a name and optional version
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct CrateRef {
+    name: Arc<str>,
+    version: Option<Arc<Version>>,
+}
+
+impl CrateRef {
+    /// Create a new crate ID with name and optional version
+    #[must_use]
+    pub fn new(name: impl AsRef<str>, version: Option<Version>) -> Self {
+        Self {
+            name: Arc::from(name.as_ref()),
+            version: version.map(Arc::new),
+        }
+    }
+
+    #[must_use]
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    #[must_use]
+    pub fn version(&self) -> Option<&Version> {
+        self.version.as_deref()
+    }
+
+    /// Get a clone of the name Arc
+    #[must_use]
+    pub fn name_arc(&self) -> Arc<str> {
+        Arc::clone(&self.name)
+    }
+
+    /// Get a clone of the version Arc if present (cheap pointer clone, no Version allocation)
+    #[must_use]
+    pub fn version_arc(&self) -> Option<Arc<Version>> {
+        self.version.as_ref().map(Arc::clone)
+    }
+
+    /// Convert to a `CrateSpec` by cloning Arc pointers (no allocation)
+    #[must_use]
+    pub fn to_spec(&self) -> Option<CrateSpec> {
+        Some(CrateSpec::from_arcs(Arc::clone(&self.name), Arc::clone(self.version.as_ref()?)))
+    }
+}
+
+impl FromStr for CrateRef {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, String> {
+        if let Some((name, version_str)) = s.split_once('@') {
+            let version =
+                Version::parse(version_str).map_err(|e| format!("invalid version '{version_str}' in crate specifier '{s}': {e}"))?;
+            Ok(Self::new(name, Some(version)))
+        } else {
+            Ok(Self::new(s, None))
+        }
+    }
+}
+
+/* Replace the above once onho gets the right impl block for AppError
+impl FromStr for CrateRef {
+    type Err = ohno::AppError;
+
+    fn from_str(s: &str) -> Result<Self> {
+        if let Some((name, version_str)) = s.split_once('@') {
+            let version =
+                Version::parse(version_str).into_app_err_with(|| format!("invalid version '{version_str}' in crate specifier '{s}'"))?;
+            Ok(Self::new(name, Some(version)))
+        } else {
+            Ok(Self::new(s, None))
+        }
+    }
+}
+*/
+
+impl Display for CrateRef {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "{}", self.name())?;
+        if let Some(version) = self.version() {
+            write!(f, "@{version}")?;
+        }
+        Ok(())
+    }
+}

src/ranking/ranker.rs
@@ -1,0 +1,101 @@
+//! Scoring logic for evaluating crate quality.
+
+use crate::config::Config;
+use crate::facts::CrateFacts;
+use crate::metrics::{Metric, MetricCategory};
+use crate::misc::DependencyType;
+use crate::ranking::{PolicyOutcome, metric_calculator};
+use compact_str::CompactString;
+use core::cell::RefCell;
+use core::hash::BuildHasher;
+use std::collections::HashMap;
+
+/// Result of ranking a crate
+#[derive(Debug)]
+pub struct RankingOutcome {
+    pub overall_score: f64,
+    pub category_scores: HashMap<MetricCategory, f64>,
+    pub details: HashMap<Metric, PolicyOutcome>,
+    pub dependency_type: DependencyType,
+}
+
+/// Ranker evaluates crate quality based on configured policies
+#[derive(Debug)]
+pub struct Ranker<'a> {
+    config: &'a Config,
+    policy_outcomes: RefCell<HashMap<Metric, PolicyOutcome>>,
+}
+
+impl<'a> Ranker<'a> {
+    /// Create a new ranker with the given configuration
+    #[must_use]
+    pub fn new(config: &'a Config) -> Self {
+        Self {
+            config,
+            policy_outcomes: RefCell::new(HashMap::new()),
+        }
+    }
+
+    /// Rank a crate based on multiple quality criteria
+    pub fn rank(&self, facts: &CrateFacts, dependency_type: DependencyType) -> RankingOutcome {
+        let mut policy_outcomes = self.policy_outcomes.borrow_mut();
+
+        policy_outcomes.clear();
+        metric_calculator::calculate(self.config, facts, dependency_type, &mut policy_outcomes);
+
+        let mut total_points = 0.0;
+        let mut category_points: HashMap<MetricCategory, f64> = HashMap::new();
+        let mut category_counts: HashMap<MetricCategory, usize> = HashMap::new();
+
+        for (metric, outcome) in policy_outcomes.iter() {
+            let category = metric.category();
+            let points = match outcome {
+                PolicyOutcome::Match(points, _info) => *points,
+                PolicyOutcome::NoMatch(_reason) => 0.0,
+            };
+            total_points += points;
+            *category_points.entry(category).or_insert(0.0) += points;
+            *category_counts.entry(category).or_insert(0) += 1;
+        }
+
+        let score = if policy_outcomes.is_empty() {
+            0.0
+        } else {
+            #[expect(clippy::cast_precision_loss, reason = "Precision loss acceptable for score calculation")]
+            let avg = total_points / policy_outcomes.len() as f64;
+            (avg * 100.0).round() / 100.0
+        };
+
+        // Compute average score per category
+        let mut category_scores = HashMap::new();
+        for (category, total_points) in category_points {
+            if let Some(&count) = category_counts.get(&category)
+                && count > 0
+            {
+                #[expect(clippy::cast_precision_loss, reason = "Precision loss acceptable for score calculation")]
+                let avg = total_points / count as f64;
+                let rounded = (avg * 100.0).round() / 100.0;
+                _ = category_scores.insert(category, rounded);
+            }
+        }
+
+        RankingOutcome {
+            overall_score: score,
+            category_scores,
+            details: policy_outcomes.clone(),
+            dependency_type,
+        }
+    }
+}
+
+/// Extract failure reasons from policy results
+#[must_use]
+pub fn extract_reasons<S: BuildHasher>(details: &HashMap<Metric, PolicyOutcome, S>) -> Vec<CompactString> {
+    let mut reasons = Vec::new();
+    for outcome in details.values() {
+        if let PolicyOutcome::NoMatch(reason) = outcome {
+            reasons.push(reason.clone());
+        }
+    }
+    reasons
+}